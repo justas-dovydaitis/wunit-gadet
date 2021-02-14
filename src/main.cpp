06d3d264946fd4f2f757671e57d8d48a4ca3849c
Justas Dovydaitis <dovydaitis-justas@gmail.com>
2021-02-14 23:19:24 +0200

    Connect angle sensor. Get angle value.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
 
+#include <wunit-angle.h>
+
+#define ANGLE_PIN 34
+#define ANGLE_D1_PIN 39
+#define ANGLE_D2_PIN 36
+
+WUnitAngle *angle;
+
 void setup()
 {
+    angle = new WUnitAngle(ANGLE_PIN, ANGLE_D1_PIN, ANGLE_D2_PIN);
 }
 
 void loop()
 {
+    // Test angle;
+    delay(1000);
+
+    Serial.print("angle: ");
+    Serial.println(angle->getAngle());
 }
